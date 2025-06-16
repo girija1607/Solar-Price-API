a2348c3ef8a4b225bb697c03657f4d0b22ea1178
Girija <girijasinghal1607@gmail.com>
2025-06-16 23:47:25 +0530

    Use process.env.PORT for Render deployment

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,8 @@ const { getBasePrice, calculateFinalPrice } = require('./utils/calculate');
 const basePrices = require('./data/basePrices.json');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 app.use(cors());
 app.use(express.json());
